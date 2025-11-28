f3f72dce47c75bbc45fdddad2ff858c08f72b19e
VanessaRibe <vanessamanaca52@gmail.com>
2025-11-28 17:27:03 -0300

    Corrigido contexto do db.create_all

--- setup_inicial.py
+++ setup_inicial.py
@@ -1,11 +1,6 @@
-# setup_inicial.py
-
-from app import create_app
-from create_admin import initialize_database
-from extensions import db
+from app import create_app, db
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-    initialize_database()
