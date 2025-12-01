70d5299b55376d69e6cc44df2146c095c0156a7c
VanessaRibe <vanessamanaca52@gmail.com>
2025-12-01 00:33:19 -0300

    Corrigido ordem de definição do app

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
 from auth import auth as auth_blueprint
+
+app = Flask(__name__)  # Primeiro: cria o app
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
-login_manager.init_app(app)
+login_manager.init_app(app)  # Agora sim: usa o app
 
 @app.route("/teste")
 @login_required
 def teste():
     return "<h1>Aplicação online</h1>"
 
-app = Flask(__name__)
 app.register_blueprint(auth_blueprint)
-app.register_blueprint(auth_blueprint)
-
