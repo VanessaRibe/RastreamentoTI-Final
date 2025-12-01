17ec9a4c1eb13c68a51eb71556e806e9e1f846ad
VanessaRibe <vanessamanaca52@gmail.com>
2025-12-01 00:49:16 -0300

    Corrigido erro 500 na rota /login

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask
-from flask_login import LoginManager, login_required
+from flask_login import LoginManager
 from auth import auth as auth_blueprint
 
-app = Flask(__name__)  # Primeiro: cria o app
+app = Flask(__name__)
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
-login_manager.init_app(app)  # Agora sim: usa o app
+login_manager.init_app(app)
 
-@app.route("/teste")
-@login_required
-def teste():
-    return "<h1>Aplicação online</h1>"
 @app.route("/")
 def index():
     return "<h1>Sistema online</h1>"
-
 
 app.register_blueprint(auth_blueprint)
