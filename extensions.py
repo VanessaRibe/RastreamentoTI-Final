e682330c689b1018246f5104099d3ca8651757bc
VanessaRibe <vanessamanaca52@gmail.com>
2025-12-01 16:36:51 -0300

    Adicionado suporte ao Flask-Migrate no extensions.py

--- extensions.py
+++ extensions.py
@@ -2,9 +2,11 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 login_manager.login_view = 'main.login'
 login_manager.login_message = 'Por favor, faça login para acessar esta página.'
