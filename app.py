b8479623b44bc9a9b7c087394a0350acbdcf372a
VanessaRibe <vanessamanaca52@gmail.com>
2025-12-01 16:56:04 -0300

    Deploy pronto: Flask-Migrate integrado, banco configurado, estrutura final aplicada

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
+import os
 from flask import Flask
-from extensions import db
-from flask_login import LoginManager
+from extensions import db, migrate, login_manager
 from models import User
 from main import main
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # ou DATABASE_URL do Render
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "chave_local_segura")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///app.db")
 
     # Inicializar extensões
     db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # Registrar blueprints
     app.register_blueprint(main)
 
     return app
 
-# Objeto app que o gunicorn vai usar
 app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
